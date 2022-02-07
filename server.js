76089f1963d91ff3c7c7d781197b9bbbe6d426d3
krayemnada <nadakrayem41@gmail.com>
2022-02-07 11:56:09 +0100

    second commit

--- server.js
+++ server.js
@@ -8,23 +8,32 @@ app.listen(PORT, (err) =>
     err ? console.log(err) : console.log(`server is runing on port ${PORT}`)
 );
 
-const Temps = new Date();
-const days = Temps.getDay();
-const hours = Temps.getHours();
-if (days >= 1 && days <= 5 && hours >= 9 && hours < 17) {
-    app.use(express.static("Public"));
+const middle = (req, res, next) => {
+    const Temps = new Date();
+    const days = Temps.getDay();
+    const hours = Temps.getHours();
+    console.log(days);
+    if (days == 1 || hours < 9 || hours > 17 || days == 6) {
+        res.send("<h1>Not available</h1>");
+        // app.use(express.static("Public"));
 
-    app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/Public/Home.html");
-    });
+        // app.get("/", (req, res) => {
+        //     res.sendFile(__dirname + "/Public/Home.html");
+        // });
 
-    app.get("/contact-us", (req, res) => {
-        res.sendFile(__dirname + "/Public/Contact.html");
-    });
+        // app.get("/contact-us", (req, res) => {
+        //     res.sendFile(__dirname + "/Public/Contact.html");
+        // });
 
-    app.get("/our-services", (req, res) => {
-        res.sendFile(__dirname + "/Public/Services.html");
-    });
-} else {
-    app.use(express.static("NotAvailable"));
-}
+        // app.get("/our-services", (req, res) => {
+        //     res.sendFile(__dirname + "/Public/Services.html");
+        // });
+    }
+    next();
+};
+
+app.use(middle);
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(PORT, (err) =>
+    err ? console.log(err) : console.log(`server is runing on port ${PORT}`)
+);
